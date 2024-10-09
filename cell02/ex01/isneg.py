8df6f6e0a7c59d85e551b26dff777e9101292c14
queen <67784591+SelfCaresex@users.noreply.github.com>
2024-10-09 11:40:31 +0700

    Create isneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+num = int(input())
+
+if num > 0: 
+    print('This number is positive')
+elif num < 0:
+    print('This number is negative')
+else:
+    print('This number is positive nad negative') 
