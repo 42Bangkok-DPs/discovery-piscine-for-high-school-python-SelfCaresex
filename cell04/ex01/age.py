f0f64e1dec3ce25e35222dd4553da33cfc165fe2
queen <67784591+SelfCaresex@users.noreply.github.com>
2024-10-09 17:51:25 +0700

    Create age.py

---
+++ cell04/ex01/age.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+age = int(input('Please tell me you age: '))
+print(f'You are currently {age} year old. ')
+age1 = age + 10
+age2 = age + 20 
+age3 = age + 30 
+
+print(f"In 10 year, you'll be {age1} year old. ")
+print(f"In 20 year, you'll be {age2} year old. ")
+print(f"In 30 year, you'll be {age3} year old. ")
