04d2fa2b02a33a44afd592600c3e26c0a367584b
queen <67784591+SelfCaresex@users.noreply.github.com>
2024-10-09 18:04:42 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+num1 = int(input("Give me the first number: "))
+num2 = int(input("Give ne the second number: "))
+print("Thank You!")
+print(f'{num1} + {num2} = {num1+num2}')
+print(f'{num1} - {num2} = {num1-num2}')
+print(f'{num1} / {num2} = {int(num1 / num2)}')
+print(f'{num1} * {num2} = {num1 * num2}')
